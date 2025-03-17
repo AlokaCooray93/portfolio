9945c99067130fcc2f506a5e243fefe70f7bf0cb
AlokaCooray93 <ps.alokacooray@gmail.com>
2025-03-17 23:44:27 +0530

    updated

--- script.js
+++ script.js
@@ -14,20 +14,27 @@ window.onscroll = () =>{
     sections.forEach(sec => {
         let top =window.scrollY;
         let offset = sec.offsetTop -100;
-        let height =sec.getAttribute('id');
+        let height = sec.offsetHeight;
+        let id =sec.getAttribute('id');
 
         if(top >=offset && top< offset+ height){
             navLinks.forEach(links => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
+            // active sections for animation on scroll
+            sec.classList.add('show-animate');
+        }
+        // if want to use animation that repeats on scroll use this
+        else{
+            // sec.classList.remove('show-animate');
         }
     });
     // sticky header
     let header = document.querySelector('header');
-    header.classList.toggle('sticky',window.scrollY > 100);
+    header.classList.toggle('sticky', window.scrollY > 100);
 
     // remove toggle icon and navbar when click navbar linksI(scroll)
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
-} 
+}  
